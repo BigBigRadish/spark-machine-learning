2e07d8dfcb4d14cf26735968b6af79530c6a0f1b
BigBigRadish <luozhukun@163.com>
2019-02-23 14:52:24 +0800

    创建一个简单的线性回归模型with spark

---
+++ chapter02/breeze/fields/GaloisField.scala
@@ -0,0 +1,14 @@
+object GaloisField {
+
+  // GF2 operations
+  def GaloisField2(): Unit = {
+    println(Zero + Zero)
+    println(One + Zero)
+    println(One + One)
+  }
+
+  def main(args: Array[String]) {
+    GaloisField2()
+  }
+
+}
